3ce6c37aaae096f6b9f7fccfd0cba55e96942149
Chris Zuber <shgysk8zer0@gmail.com>
2018-05-30 13:55:15 -0700

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,44 @@
+import './std-js/shims.js';
+import './std-js/deprefixer.js';
+import {loaded, $, wait, registerServiceWorker} from './std-js/functions.js';
+import webShareApi from './std-js/webShareApi.js';
+import {
+	facebook,
+	twitter,
+	googlePlus,
+	linkedIn,
+	reddit,
+	gmail,
+} from './std-js/share-config.js';
+
+webShareApi(facebook, twitter, googlePlus, linkedIn, reddit, gmail);
+
+loaded().then(async () => {
+	if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
+		registerServiceWorker(document.documentElement.dataset.serviceWorker);
+	}
+
+	$('[data-show-modal]').click(event => {
+		const target = event.target.closest('[data-show-modal]');
+		const dialog = document.querySelector(target.dataset.showModal);
+		if (dialog instanceof HTMLElement) {
+			dialog.showModal();
+		}
+	});
+	$('[data-close]').click(event => {
+		const target = event.target.closest('[data-close]');
+		const dialog = document.querySelector(target.dataset.close);
+		if (dialog instanceof HTMLElement) {
+			dialog.close();
+		}
+	});
+	$('[data-share]').click(event => {
+		event.preventDefault();
+		event.stopPropagation();
+		navigator.share({
+			title: document.title,
+			url: location.href,
+			text: document.querySelector('meta[name="description"][content]').getAttribute('content'),
+		});
+	});
+});
