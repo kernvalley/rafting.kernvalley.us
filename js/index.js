11c41ba73fc397b1a8234c201985df656d2c5edc
Chris Zuber <shgysk8zer0@gmail.com>
2021-02-25 11:24:25 -0800

    Update to latest versions of shared / common assets

--- js/index.js
+++ js/index.js
@@ -1,48 +1,60 @@
 import 'https://cdn.kernvalley.us/js/std-js/deprefixer.js';
 import 'https://cdn.kernvalley.us/js/std-js/shims.js';
-import 'https://unpkg.com/@webcomponents/custom-elements@1.4.1/custom-elements.min.js';
+import 'https://cdn.kernvalley.us/js/std-js/theme-cookie.js';
 import 'https://cdn.kernvalley.us/components/share-button.js';
 import 'https://cdn.kernvalley.us/components/share-to-button/share-to-button.js';
-import 'https://cdn.kernvalley.us/components/slide-show/slide-show.js';
 import 'https://cdn.kernvalley.us/components/github/user.js';
 import 'https://cdn.kernvalley.us/components/current-year.js';
-import 'https://cdn.kernvalley.us/components/bacon-ipsum.js';
-import 'https://cdn.kernvalley.us/components/leaflet/map.js';
-import 'https://cdn.kernvalley.us/components/leaflet/marker.js';
 import 'https://cdn.kernvalley.us/components/pwa/install.js';
+import 'https://cdn.kernvalley.us/components/ad/block.js';
+import 'https://cdn.kernvalley.us/components/app/list-button.js';
+import 'https://cdn.kernvalley.us/components/app/stores.js';
 import { $, ready } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+import { init } from 'https://cdn.kernvalley.us/js/std-js/data-handlers.js';
+import { importGa, externalHandler, telHandler, mailtoHandler } from 'https://cdn.kernvalley.us/js/std-js/google-analytics.js';
+import { GA } from './consts.js';
 
-document.documentElement.classList.replace('no-js', 'js');
-document.documentElement.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
-document.documentElement.classList.toggle('no-details', document.createElement('details') instanceof HTMLUnknownElement);
+$(':root').css({'--viewport-height': `${window.innerHeight}px`});
 
-ready().then(async () => {
-	$('[data-scroll-to]').click(event => {
-		const target = document.querySelector(event.target.closest('[data-scroll-to]').dataset.scrollTo);
-		target.scrollIntoView({
-			bahavior: 'smooth',
-			block: 'start',
+requestIdleCallback(() => {
+	$(window).debounce('resize', () => $(':root').css({'--viewport-height': `${window.innerHeight}px`}));
+
+	$(window).on('scroll', () => {
+		requestAnimationFrame(() => {
+			$('#header').css({
+				'background-position-y': `${-0.5 * scrollY}px`,
+			});
+		});
+	}, { passive: true });
+});
+
+$(document.documentElement).toggleClass({
+	'no-dialog': document.createElement('dialog') instanceof HTMLUnknownElement,
+	'no-details': document.createElement('details') instanceof HTMLUnknownElement,
+	'js': true,
+	'no-js': false,
+});
+
+if (typeof GA === 'string' && GA.length !== 0) {
+	requestIdleCallback(() => {
+		importGa(GA).then(async ({ ga }) => {
+			if (ga instanceof Function) {
+				ga('create', GA, 'auto');
+				ga('set', 'transport', 'beacon');
+				ga('send', 'pageview');
+
+				await ready();
+
+				$('a[rel~="external"]').click(externalHandler, { passive: true, capture: true });
+				$('a[href^="tel:"]').click(telHandler, { passive: true, capture: true });
+				$('a[href^="mailto:"]').click(mailtoHandler, { passive: true, capture: true });
+			}
 		});
 	});
+}
 
-	$('[data-show]').click(event => {
-		const target = document.querySelector(event.target.closest('[data-show]').dataset.show);
-		if (target instanceof HTMLElement) {
-			target.show();
-		}
-	});
-
-	$('[data-show-modal]').click(event => {
-		const target = document.querySelector(event.target.closest('[data-show-modal]').dataset.showModal);
-		if (target instanceof HTMLElement) {
-			target.showModal();
-		}
-	});
-
-	$('[data-close]').click(event => {
-		const target = document.querySelector(event.target.closest('[data-close]').dataset.close);
-		if (target instanceof HTMLElement) {
-			target.tagName === 'DIALOG' ? target.close() : target.open = false;
-		}
-	});
+Promise.allSettled([
+	ready(),
+]).then(() => {
+	init().catch(console.error);
 });
